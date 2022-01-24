552aaf3bf39f930ab27f3830a4c7fad88e977ffa
unknown <davisoares1999@hotmail.com>
2022-01-24 18:40:32 -0300

    upando desafio04 vai na web

---
+++ script.js
@@ -0,0 +1,31 @@
+/* - - QUESTÃO 1 - - */
+function texto(){
+    return ("Olá mundo")
+}
+console.log(texto())
+
+/* - - QUESTÃO 2 - - */
+function meuNome(nome){
+    console.log(`Meu nome é ${nome}`)
+}
+meuNome("Davi Soares")
+
+/* - - QUESTÃO 3 - - */
+function pesquisa(name, idade, estilo){
+    console.log(`Meu nome é ${name} estou com ${idade} e prefiro o estilo musica ${estilo}`)
+}
+pesquisa("Davi","22","RAP")
+
+/* - - QUESTÃO 4 - - */
+function pesquisa2(filme, musica){
+    console.log(`Filme: ${filme}  Musica: ${musica}`)
+}
+pesquisa2("Os Vingadores","The Weeknd - Save your tears")
+
+/* - - QUESTÃO 5 - - */
+function triplo(k){
+    k = k*3
+    return(console.log(k))
+}
+triplo(25)
+
